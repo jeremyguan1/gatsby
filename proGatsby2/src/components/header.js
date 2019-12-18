7bdce7519c8ce3f85a04c606e9ffbe0dead7ee35
Jeremy Guan <jeremyguan1@gmail.com>
2019-12-17 18:10:30 -0800

    init

--- proGatsby2/src/components/header.js
+++ proGatsby2/src/components/header.js
@@ -2,6 +2,8 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import gatsbyLogo from "../images/gatsby-icon.png"
+
 const Header = ({ siteTitle }) => (
   <header
     style={{
@@ -24,7 +26,7 @@ const Header = ({ siteTitle }) => (
             textDecoration: `none`,
           }}
         >
-          {siteTitle}
+          <img src={gatsbyLogo} alt="Gatsby Logo" style={{ width: "100px" }} />
         </Link>
       </h1>
     </div>
---
+++ proGatsby2/src/pages/company/team.js
@@ -0,0 +1,18 @@
+import React from "react"
+import Layout from "../../components/layout"
+
+const Team = () => (
+  <Layout>
+    <div>
+      <h1>The Team</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+        inventore nobis odit cum repellat doloremque reiciendis accusamus
+        blanditiis quo voluptas. Eaque, voluptatum? Nostrum porro eos rem
+        perferendis quo qui quod.
+      </p>
+    </div>
+  </Layout>
+)
+
+export default Team
